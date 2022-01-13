553a40a7ae18fd5ecbbf7ef98989bdc45fb263d1
BehshadBabai <behshad.babai@gmail.com>
2022-01-13 00:52:47 -0800

    implementing the app and contactsPage components

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,33 +1,50 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import { TileList } from "../../components/tileList/TileList.js";
+import { ContactForm } from "../../components/contactForm/ContactForm.js";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+export const ContactsPage = (props) => {
     /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
+    Define state variables for 
+    contact info and duplicate check
     */
-  };
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+    if (props.contacts.includes(name)) {
+        alert(`You won't be able to add this contact with ${name} as name since duplicate contacts are not allowed in this form!`);
+    }
 
-  return (
-    <div>
-      <section>
-        <h2>Add Contact</h2> 
-      </section>
-      <hr />
-      <section>
-        <h2>Contacts</h2>
-      </section>
-    </div>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /*
+        Add contact info and clear data
+        if the contact name is not a duplicate
+        */
+        if (!props.contacts.includes(name)) {
+            props.addContacts(name, phone, email);
+            setName('');
+            setPhone('');
+            setEmail('');
+        }
+    };
+
+    /*
+    Using hooks, check for contact name in the 
+    contacts array variable in props
+    */
+    
+    return (
+        <div>
+            <section>
+                <h2>Add Contact</h2>
+                <ContactForm onSubmit={handleSubmit} name={name} phone={phone} email={email} setName={setName} setPhone={setPhone} setEmail={setEmail} />
+            </section>
+            <hr />
+            <section>
+                <h2>Contacts</h2>
+                <TileList contacts={props.contacts} />
+            </section>
+        </div>
+    );
 };
